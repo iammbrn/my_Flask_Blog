6e52461e8ae2e350834ab6b6a7164a38f6004fac
iammbrn <iammbaran@gmail.com>
2025-06-09 01:17:55 +0300

    Added new route handlers and updated.

--- my_blog.py
+++ my_blog.py
@@ -73,9 +73,23 @@ def user():
 @login_required
 def dashboard():
     numbers = [1, 2, 3]
-    return render_template("dashboard.html", numbers = numbers)
+    
+    cursor = mysql.connection.cursor()
 
+    query = "SELECT * From articles WHERE author = %s"  
 
+    cursor.execute(query, (session["username"],))
+
+    articles = cursor.fetchall()
+
+    cursor.close()
+
+    if articles:
+        return render_template("dashboard.html", articles = articles)
+
+    else:
+        return render_template("dashboard.html")
+    
 @app.route("/register", methods = ["GET", "POST"])
 def register():
 
@@ -195,15 +209,152 @@ def articles():
 
     articles = cursor.fetchall()
 
+    cursor.close()
+
     if articles:
         return render_template("articles.html", articles = articles)
     
     else:
         return render_template("articles.html")
 
-@app.route("/article/<string:id>") #Dinamik URL yapısı ve kullanımı.
-def article(id):
-    return "Article id:" + id
+@app.route("/viewarticle/<string:id>") #Dinamik URL yapısı ve kullanımı.
+def viewarticle(id):
+    cursor = mysql.connection.cursor()
+
+    query = "SELECT * FROM articles WHERE id = %s"
+
+    cursor.execute(query, (id, ))
+
+    article = cursor.fetchone()
+
+    cursor.close()
+
+    if article:
+        return render_template("viewarticle.html", article = article)
+
+    else: 
+        return render_template("viewarticle.html")
+
+
+@app.route("/editarticle/<string:id>", methods = ["GET", "POST"])
+@login_required
+def editarticle(id):
+    
+    cursor = mysql.connection.cursor()
+
+    existquery = "SELECT * FROM articles WHERE id = %s and author = %s"
+
+    cursor.execute(existquery, (id, session["username"]))
+
+    article = cursor.fetchone()
+
+
+    if request.method == "GET":
+
+        if article:
+            form = ArticleForm()
+
+            form.title.data = article["title"]
+            form.content.data = article["content"]
+            cursor.close()
+            return render_template("editarticle.html", form = form)
+
+
+        else:
+            flash("Böyle bir makaleniz yok!", "danger")
+            return redirect(url_for("index"))
+        
+    else:
+        # POST request 
+
+        if article:
+
+            form = ArticleForm(request.form)
+
+            newtitle = form.title.data
+            newcontent = form.content.data
+
+            updatequery = "UPDATE articles SET title = %s, content = %s WHERE id = %s"
+
+            cursor.execute(updatequery, (newtitle, newcontent, id))
+
+            mysql.connection.commit()
+
+            cursor.close()
+
+            flash("Makaleniz başarıyla güncellendi.", "success")
+
+            return redirect(url_for("dashboard"))
+        
+        else:
+            flash("Böyle bir makaleniz yok!", "danger")
+            return redirect(url_for("index"))
+
+
+
+        
+
+
+@app.route("/deletearticle/<string:id>")
+@login_required
+def deletearticle(id):
+    cursor = mysql.connection.cursor()
+
+    existquery = "SELECT * FROM articles WHERE id = %s and author = %s"
+
+    cursor.execute(existquery, (id, session["username"]))
+
+    article = cursor.fetchone()
+
+    if article:
+
+        deletequery = "DELETE FROM articles WHERE id = %s"
+
+        cursor.execute(deletequery, (id, ))
+
+        mysql.connection.commit()
+
+        cursor.close()
+
+        flash("Makale başarıyla silindi.", "success")
+
+        return redirect(url_for("dashboard"))
+    
+    else:
+        cursor.close()
+        flash("Silinecek böyle bir makaleniz bulunmadı!", "danger")
+        return redirect(url_for("index"))
+    
+
+
+@app.route("/search", methods = ["GET", "POST"])
+def search():
+
+    if request.method == "GET":
+        return redirect(url_for("index"))
+    
+    else:
+        # POST request
+
+        keyword = request.form.get("keyword")
+
+        cursor = mysql.connection.cursor()
+
+        #Return all articles from a title that contains the letter keyword value:
+        searchquery = "SELECT * FROM articles WHERE title LIKE '%" + keyword + "%'" 
+        
+        cursor.execute(searchquery)
+
+        articles = cursor.fetchall()
+
+        cursor.close()
+
+        if articles:
+            return render_template("articles.html", articles = articles)
+
+        else:
+            flash("Aranan sorguya uygun makale bulunamadı.", "warning")
+            return redirect(url_for("articles"))
 
 
 if __name__ == "__main__":
